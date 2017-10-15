82a6f3a4b85a73828eb96e981d8b1c33eff90f34
fajarbardan <fajarbardan@gmail.com>
2017-10-15 19:58:11 +0700

    push tutorial 6

--- APAPTutorial05/src/main/java/com/example/controller/StudentController.java
+++ APAPTutorial05/src/main/java/com/example/controller/StudentController.java
@@ -41,7 +41,7 @@ public class StudentController
             @RequestParam(value = "gpa", required = false) double gpa)
     {
         StudentModel student = new StudentModel (npm, name, gpa, null);
-        studentDAO.addStudent (student);
+    	studentDAO.addStudent (student);
 
         return "success-add";
     }
